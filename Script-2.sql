a26fba619d01278177e00813cb2aaf5ca687426d
Egorov Andrey <134514892+InfernalBeard@users.noreply.github.com>
2024-02-10 19:27:31 +0300

    Update Script-2.sql

--- Script-2.sql
+++ Script-2.sql
@@ -1,2 +1,6 @@
+/*
+Напишите скрипт, который будет искать в таблице PERSONS поля name и surname пользователей, которые проживают в MOSCOW.
+*/
+
 SELECT name, surname FROM netology.persons
 WHERE city_of_living = 'MOSCOW';
