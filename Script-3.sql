5640eda687d4a134b8e4ea01994bacad6145b37b
Egorov Andrey <134514892+InfernalBeard@users.noreply.github.com>
2024-02-10 19:28:35 +0300

    Update Script-3.sql

--- Script-3.sql
+++ Script-3.sql
@@ -1,3 +1,8 @@
-select * from netology.persons
-where age > 27
-order by age;
+/*
+Напишите скрипт, который будет искать в таблице PERSONS все поля, у которых поле age выше 27 лет.
+Отсортируйте получаемые результаты по убыванию возраста.
+*/
+
+SELECT * FROM PERSONS
+         WHERE age > 27
+         ORDER BY age;
